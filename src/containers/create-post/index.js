0c827a565e8e5efcf3380ac1a296c6e6049e6327
patfitz95 <patfitz95@gmail.com>
2021-05-22 14:58:29 -0500

    added some functionality to the post section

--- src/containers/create-post/index.js
+++ src/containers/create-post/index.js
@@ -3,14 +3,59 @@ import { SignInBtn } from '../../components'
 import { UserContext } from '../../context/user'
 import './style.css'
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+import makeid from '../../helpers/functions';
+import { db, storage } from '../../firebase';
+import firebase from "firebase"
 
 export default function CreatePost() {
 
 const [user, setUser] = useContext(UserContext).user;
 const [caption, setCaption] = useState("");
-const handleChange = () => {}
-const handleUpload = () => {
 
+const [image, setImage] = useState(null);
+
+const [progress, setProgress] = useState(0);
+
+const handleChange = (e) => {
+    if(e.target.files[0]){
+        setImage(e.target.files[0]);
+
+        let selectedImageSrc = URL.createObjectURL(e.target.files[0]);
+
+        let imagePreview = document.getElementById('image-preview');
+        imagePreview.src = selectedImageSrc;
+        imagePreview.style.display = "block"
+    }
+}
+const handleUpload = () => {
+    if(image) {
+        var imageName = makeid(10);
+        const uploadTask = storage.ref(`images/${imageName}.jpg`).put(image);
+
+        uploadTask.on('state_changed',(snapshot) => {
+            // progress function
+
+            const progress = Math.round((snapshot.bytesTransferred/snapshot.totalBytes)*100)
+
+            setProgress(progress);
+        }, (error) => {
+            console.log(error)
+        },() => {
+            // get download url and upload the post info
+
+            storage.ref("images").child(`${imageName}.jpg`).getDownloadURL().then((imageUrl) => {
+
+                db.collection("posts").add({
+                    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                    caption: caption,
+                    photoUrl: imageUrl,
+                    username: user.email.replace("@gmail.com",""),
+                    profileUrl: user.photoURL,
+
+                })
+            })
+        })
+    }
 }
 
     return (
@@ -19,12 +64,18 @@ const handleUpload = () => {
                 <p>Create a Post</p>
                 <div className="createPost_loggedInCenter">
                     <textarea className="createPost_textarea"
-                     rows="3" 
+                     rows="3"
+                     placeholder="enter caption here.."
                      value={caption}
                      onChange={(e) => setCaption(e.target.value)}>
 
                     </textarea>
+
+                    <div className="createPost_imagePreview">
+                        <img id="image-preview" alt=""></img>
+                    </div>
                 </div>
+                <div className="createPost_loggedInBottom">
                 <div className="createPost_imageUpload">
                     <label htmlFor="fileInput">
                     <AddAPhotoIcon style={{cursor:"pointer", fontSize: "20px"}}/>
@@ -33,7 +84,8 @@ const handleUpload = () => {
                 </div>
                 <button className="createPost_uploadBtn" 
                 onClick={handleUpload} style={{color: caption ? "#000" : "lightgrey"}}>
-                    Upload</button>
+                    {`Upload ${progress != 0 ? progress : ""}`}</button>
+                    </div>
                 </div>) : ( <div>
                 <SignInBtn/>
                 <p style={{marginLeft: "12px"}}>to Post & Comment</p>
